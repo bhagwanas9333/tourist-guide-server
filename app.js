24e116b65bd2815cb5294f95276a7e04c095203d
bhagwan sonawane <bhagwans9333@gmail.com>
2023-11-23 17:11:03 +0530

    server app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config({
+  path: "./config/.env",
+});
+const app = express();
+
+app.use(cors());
+app.use(express.static("uploads"));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  console.log("req.body..", req.body);
+  //access client to access the "x-token" to header
+  res.header("Access-Control-Expose-Headers", "x-accesstoken,x-refreshtoken");
+  next();
+});
+
+const port = process.env.PORT || 9090;
+
+// http://localhost:8080/api/v1
+// app.use("/api/v1", require("./v1"));
+app.get("/",(req,res)=>{
+    res.status(200).send("welcome to sever")
+})
+app.listen(port, () => {
+  console.log(`severe is listing on port ${port}`);
+});
