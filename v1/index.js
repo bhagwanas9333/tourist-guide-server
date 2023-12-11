d3281775e3ecf6388155bdd202d95634668cddca
priya-bhondwe <149312604+priya-bhondwe@users.noreply.github.com>
2023-12-11 22:10:31 +0530

    in index hotel , restro route deleted

--- v1/index.js
+++ v1/index.js
@@ -3,8 +3,6 @@ require("./models/db");
 router.use("/users", require("./routes/user.route"));
 router.use("/auth", require("./routes/auth.route"));
 router.use("/places", require("./routes/place.route"));
-router.use("/hotels", require("./routes/hotel.route"));
-router.use("/restaurants", require("./routes/restaurant.route"));
 router.use("/data", require("./routes/data.route"));
 
 module.exports = router;
