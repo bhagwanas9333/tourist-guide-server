bc62bbfbdab5f54cd59b0bccbc27d36fc54ebeea
priya-bhondwe <149312604+priya-bhondwe@users.noreply.github.com>
2023-12-11 11:09:13 +0530

    hotel and restaurant deleted
    hotel and restaurant deleted

--- v1/index.js
+++ v1/index.js
@@ -3,7 +3,4 @@ require("./models/db");
 router.use("/users", require("./routes/user.route"));
 router.use("/auth", require("./routes/auth.route"));
 router.use("/places", require("./routes/place.route"));
-router.use("/hotels", require("./routes/hotel.route"));
-router.use("/restaurants", require("./routes/restaurant.route"));
-
 module.exports = router;
